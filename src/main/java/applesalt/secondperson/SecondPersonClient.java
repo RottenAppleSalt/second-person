99dcd6961584c51f2e26823ee5b18a9fdcc43a06
Noa <rottenapplesalt@gmail.com>
2024-12-09 20:05:50 -0600

    initial commit

---
+++ src/main/java/applesalt/secondperson/SecondPersonClient.java
@@ -0,0 +1,10 @@
+package applesalt.secondperson;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SecondPersonClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
